f1f03014b3e129ac104fd7d212082b39cd2d9786
CHIA CHIANG <105509117@mail2.nccu.tw>
2018-10-11 01:20:52 +0800

    Initial commit

---
+++ SUN.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SUN : MonoBehaviour {
+    public float y;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y, 0);
+        transform.Rotate(angle);
+   
+
+    }
+}
